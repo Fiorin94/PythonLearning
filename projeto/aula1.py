4c88bc41050a0efb95f4a6fe1238d1a68ea27a13
LordFioros <felipefiorin94@gmail.com>
2023-06-27 19:01:42 -0300

    Primeiras aulas

---
+++ projeto/aula1.py
@@ -0,0 +1,6 @@
+"""
+Docstrings se escrevem com três aspas. Porém não são ignorados pelo interpretador de código.
+
+"""
+#  Comentários são realizados pela hashtag
+print ('Hello, World') # Hello world para dar sorte.
---
+++ projeto/aula2.py
@@ -0,0 +1,10 @@
+"""
+Existem argumentos que podemos colocar nas chamadas.
+Como 'sep=' para definir o tipo de separação dos argumentos
+E também o 'end=' para definir o que será incluso após o último argumento.
+
+Como padrão, ele inclui um espaço para separar e a quebra de linha no final.
+"""
+
+print(12, 34, sep='-', end= '\n')
+print(56, 78,sep='-', end= '\n')
