10de101296237fe0b1058fcc046631e1f6d4fc0f
LordFioros <felipefiorin94@gmail.com>
2023-08-10 15:44:05 -0300

    Dictionary exercise

--- projeto/exercicio_questionario.py
+++ projeto/exercicio_questionario.py
@@ -17,3 +17,38 @@ perguntas = [
         'Resposta': '5',
     },
 ]
+qtd_acertos = 0
+for pergunta in perguntas:
+    print('Pergunta:', pergunta['Pergunta'])
+    print()
+
+    opcoes = pergunta['Opções']
+    for i, opcao in enumerate(opcoes):
+        print(f'{i})', opcao)
+    print()
+    
+    escolha = input('Escolha uma opção: ')
+
+    acertou = False
+    escolha_int = None
+    qtd_opcoes = len(opcoes)
+
+    if escolha.isdigit():
+        escolha_int = int(escolha)
+    else:
+        print('Digite apenas uma das opções.')
+        break
+
+    if escolha_int is not None:
+        if escolha_int >= 0  and escolha_int <= qtd_opcoes:
+            if opcoes[escolha_int] == pergunta['Resposta']:
+                acertou = True
+    
+    if acertou:
+        qtd_acertos += 1
+        print('Resposta Correta.')
+    else:
+        print('Errou.')
+
+    print()
+print(f'Você acertou {qtd_acertos} de {len(perguntas)} perguntas.')
