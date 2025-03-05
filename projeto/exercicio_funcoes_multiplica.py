dc844de2456878e6141a2699a2927194acfb6bba
LordFioros <felipefiorin94@gmail.com>
2025-03-05 16:40:49 -0300

    Refactoring codes

--- projeto/exercicio_funcoes_multiplica.py
+++ projeto/exercicio_funcoes_multiplica.py
@@ -5,10 +5,24 @@ def criar_multiplicador(multiplicador):
         return numero * multiplicador
     return multiplicar
 
-duplicar = criar_multiplicador(5)
+duplicar = criar_multiplicador(2)
 triplicar = criar_multiplicador(3)
 quadruplicar = criar_multiplicador(4)
 
 print(duplicar(2))
 print(triplicar(2))
 print(quadruplicar(2))
+
+
+def par_impar(numero):
+    multiplo_de_dois = numero % 2 == 0
+
+    if multiplo_de_dois:
+        return f'{numero} é par'
+    else:
+        return f'{numero} é ímpar'
+    
+
+print( par_impar(2))
+print(par_impar(15))
+print(par_impar(4))
